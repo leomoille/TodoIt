9d0d28af319c9c216f9d7b706835394610ee4e62
Léo Moille <leomoille18@gmail.com>
2023-08-20 15:53:41 +0200

    :art: php cs fixer!

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -48,4 +47,3 @@ class RegistrationController extends AbstractController
         ]);
     }
 }
-
